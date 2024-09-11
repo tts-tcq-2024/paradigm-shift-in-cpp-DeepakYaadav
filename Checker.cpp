50843c5666c5fd2bfb840e9143f4336bef350695
Deepak Kumar Yadav <yadav1996deepak1@gmail.com>
2024-09-11 23:04:47 +0530

    Update

--- Checker.cpp
+++ Checker.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 using namespace std;
 
@@ -9,27 +10,42 @@ enum BatteryStatus {
     CHARGE_RATE_OUT_OF_RANGE
 };
 
-// Function to check if value is in range
-BatteryStatus checkInRange(float value, float min, float max, BatteryStatus outOfRangeStatus) {
-    return (value < min || value > max) ? outOfRangeStatus : BATTERY_OK;
+// Function to check if a value is within a range
+BatteryStatus checkInRange(float value, float min, float max) {
+    return (value < min || value > max) ? TEMPERATURE_OUT_OF_RANGE : BATTERY_OK;
+}
+
+// Function to check temperature limits
+BatteryStatus checkTemperature(float temperature) {
+    return checkInRange(temperature, 0, 45);
+}
+
+// Function to check SOC limits
+BatteryStatus checkSoc(float soc) {
+    return checkInRange(soc, 20, 80);
+}
+
+// Function to check charge rate limits
+BatteryStatus checkChargeRate(float chargeRate) {
+    return checkInRange(chargeRate, 0, 0.8);
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-    // Check all conditions in a single decision point
-    BatteryStatus status = BATTERY_OK;
-    if (checkInRange(temperature, 0, 45, TEMPERATURE_OUT_OF_RANGE) != BATTERY_OK ||
-        checkInRange(soc, 20, 80, SOC_OUT_OF_RANGE) != BATTERY_OK ||
-        checkInRange(chargeRate, 0, 0.8, CHARGE_RATE_OUT_OF_RANGE) != BATTERY_OK) {
-        // Determine which error occurred
-        if (checkInRange(temperature, 0, 45, TEMPERATURE_OUT_OF_RANGE) != BATTERY_OK) {
-            cout << "Temperature out of range!\n";
-        } else if (checkInRange(soc, 20, 80, SOC_OUT_OF_RANGE) != BATTERY_OK) {
-            cout << "State of Charge out of range!\n";
-        } else if (checkInRange(chargeRate, 0, 0.8, CHARGE_RATE_OUT_OF_RANGE) != BATTERY_OK) {
-            cout << "Charge Rate out of range!\n";
-        }
+    if (checkTemperature(temperature) != BATTERY_OK) {
+        cout << "Temperature out of range!\n";
         return false;
     }
+
+    if (checkSoc(soc) != BATTERY_OK) {
+        cout << "State of Charge out of range!\n";
+        return false;
+    }
+
+    if (checkChargeRate(chargeRate) != BATTERY_OK) {
+        cout << "Charge Rate out of range!\n";
+        return false;
+    }
+
     return true;
 }
 
